96889b188f82418e587a5d0cc8ed7101c9500f2b
gudev-db <33608430+gudev-db@users.noreply.github.com>
2024-01-27 15:08:28 +0000

    potir

--- app.py
+++ app.py
@@ -2,38 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  
-    {
-      'id': 1,
-      'title': 'Flores',
-      'location': 'Bengaluru, India',
-      'salary': 'Rs. 10,00,000'
-    },
-    {
-      'id': 2,
-      'title': 'Data Scientist',
-      'location': 'Delhi, India',
-      'salary': 'Rs. 15,00,000'
-    },
-    {
-      'id': 3,
-      'title': 'Frontend Engineer',
-      'location': 'Remote',
-      'salary': 'Rs. 12,00,000'
-    },
-    {
-      'id': 4,
-      'title': 'Backend Engineer',
-      'location': 'San Francisco, USA',
-      'salary': '$120,000'
-    }
-  ]
+JOBS = [{
+    'id': 1,
+    'title': 'Flores',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html', jobs = JOBS, carreer = 'Potira')
+  return render_template('home.html', jobs=JOBS, carreer='Potir')
+
 
 @app.route('/api/jobs')
 def list_jobs():
@@ -41,5 +36,5 @@ def list_jobs():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug = True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
   hello_world()
